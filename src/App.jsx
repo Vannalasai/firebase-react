d498ecf6eff15bc9feea356ae08e6ec9101a6723
vannala sai <saikrishna.vannala@gmail.com>
2024-11-11 23:11:43 +0530

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react"
+import { Routes , Route } from 'react-router-dom'
 import Login from "./components/Login"
+import Home from "./components/Home"
+import Register from "./components/Register"
 
 function App() {
 
   return (
     <>
-      <Login />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="login" element={<Login />}/>
+        <Route path="register" element={<Register />}/>
+      </Routes>
     </>
   )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,6 +5,10 @@ import "./css/Login.css"
 const Register = () => {
     return (
       <div className="login-page">
+        <div className="fullname">
+          <Input placeholder="First Name" type="text"/>
+          <Input placeholder="Last Name" type="text"/>
+        </div>
         <Input placeholder="Email Address" type="email"/>
         <Input placeholder="Enter Password" type="password"/>
         <Button type="primary">Register now!</Button>
