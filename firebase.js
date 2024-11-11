590f9ab1a4e0af5bb4189020c03b7581ae3f5b60
vannala sai <saikrishna.vannala@gmail.com>
2024-11-11 22:26:36 +0530

    1

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClTltjYTstSbq0wJicvg-3qJYhpY4gdMY",
+  authDomain: "react-firebase-75fed.firebaseapp.com",
+  projectId: "react-firebase-75fed",
+  storageBucket: "react-firebase-75fed.firebasestorage.app",
+  messagingSenderId: "154330236904",
+  appId: "1:154330236904:web:09d8b08e6e34537bbe065d"
+};
+
+const app = initializeApp(firebaseConfig);
