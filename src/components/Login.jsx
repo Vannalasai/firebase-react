da1575933336168e64399867c51989deeefeb076
vannala sai <saikrishna.vannala@gmail.com>
2024-11-13 23:57:28 +0530

    7

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 import { Input , Button } from "antd";
 import "./css/Login.css"
 
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 const Login = () => {
 
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+
+  async function handleClick() {
+    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+      console.log(userCredential)
+    })
+  }
+
     return (
       <div className="login-page">
-        <Input placeholder="Email Address" type="email"/>
-        <Input placeholder="Enter Password" type="password"/>
-        <Button type="primary">Login now!</Button>
+        <Input onChange={(e) => setEmail(e.currentTarget.value)} placeholder="Email Address" type="email"/>
+        <Input onChange={(e) => setPassword(e.currentTarget.value)} placeholder="Enter Password" type="password"/>
+        <Button onClick={handleClick} type="primary">Login now!</Button>
       </div>
     )
 }
