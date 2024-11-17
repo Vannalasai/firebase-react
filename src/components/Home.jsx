3c2591fe89541d8d0d288440e3564a38521620ea
vannala sai <saikrishna.vannala@gmail.com>
2024-11-18 00:04:07 +0530

    13

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useMemo, useReducer } from "react";
 
 import { db } from "../firebase";
 import { collection, getDocs, doc, getDoc, addDoc, query, where } from "firebase/firestore";
 
+const initialState = { 
+    data: [], 
+    loading: true, 
+    error: null, 
+}; 
+
+function reducer(state, action) {
+    switch (action.type) { 
+        case 'FETCH_SUCCESS': 
+            return {
+                ...state, 
+                data: action.payload, 
+                loading: false, 
+            }; 
+            case 'FETCH_ERROR': 
+            return { 
+                ...state, 
+                error: action.payload, 
+                loading: false, 
+            }; 
+            default: 
+            return state; 
+        } }
+
 const Home = () => {
 
-  useEffect(() => {
-    const firebaseFunction = async () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-    const collectionReference = collection(db, "products")
-    const q = query(collectionReference, where('price', '<', 500), where('category', '==', 'jewellery'))
-    getDocs(q).then(response => {
-        response.forEach(price => {
-            console.log(price.data().price)
-        })
-    })
+    useEffect(() => {
+        const firebaseFunction = async () => {
+
+        try{
+            const collectionReference = collection(db, "products")
+            const q = query(collectionReference, where('price', '<', 500), where('category', '==', 'jewelery'))
+            getDocs(q).then(response => {
+                response.forEach(price => {
+                    console.log(price.data().price)
+                })
+            })
+            dispatch({ type: 'FETCH_SUCCESS', payload: q });
+        } catch (error) {
+            dispatch({ type: 'FETCH_ERROR', payload: error.message});
+        }
+        }
+
+        firebaseFunction()
+    }, [])
+
+    const { q, loading, error } = state;
+
+    if (loading) return console.log('loading...'); if (error) return console.log('Error');
+
+    return(
+        <div>Home</div>
+    )
     }
-
-    firebaseFunction()
-  })
-
-  return(
-    <div>Home</div>
-  )
-}
 
 export default Home;
