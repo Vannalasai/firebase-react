4e73cc7c6ffce6c8381852a3361b67779fef888e
vannala sai <saikrishna.vannala@gmail.com>
2024-11-18 22:40:00 +0530

    13

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react"
-import { Routes , Route } from 'react-router-dom'
+import React, { useEffect } from "react"
+import { Routes , Route, useNavigate } from 'react-router-dom'
 import Login from "./components/Login"
 import Home from "./components/Home"
 import Register from "./components/Register"
 
+import { auth } from "./firebase"
+import { onAuthStateChanged } from "firebase/auth"
+
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if(user){
+        navigate('/')
+        console.log("user is login")
+      }else{
+        navigate('/login')
+        console.log('user is not there')
+      }
+    })
+  }, [])
 
   return (
     <>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,63 +1,40 @@
-import React, { useCallback, useEffect, useMemo, useReducer } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 
-import { db } from "../firebase";
-import { collection, getDocs, doc, getDoc, addDoc, query, where } from "firebase/firestore";
-
-const initialState = { 
-    data: [], 
-    loading: true, 
-    error: null, 
-}; 
-
-function reducer(state, action) {
-    switch (action.type) { 
-        case 'FETCH_SUCCESS': 
-            return {
-                ...state, 
-                data: action.payload, 
-                loading: false, 
-            }; 
-            case 'FETCH_ERROR': 
-            return { 
-                ...state, 
-                error: action.payload, 
-                loading: false, 
-            }; 
-            default: 
-            return state; 
-        } }
+import { auth, db } from "../firebase";
+import { collection, getDocs, doc, getDoc, addDoc, query, where, limit } from "firebase/firestore";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
 
 const Home = () => {
 
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const navigate = useNavigate()
 
-    useEffect(() => {
-        const firebaseFunction = async () => {
-
-        try{
-            const collectionReference = collection(db, "products")
-            const q = query(collectionReference, where('price', '<', 500), where('category', '==', 'jewelery'))
-            getDocs(q).then(response => {
-                response.forEach(price => {
-                    console.log(price.data().price)
-                })
-            })
-            dispatch({ type: 'FETCH_SUCCESS', payload: q });
-        } catch (error) {
-            dispatch({ type: 'FETCH_ERROR', payload: error.message});
-        }
-        }
-
-        firebaseFunction()
-    }, [])
-
-    const { q, loading, error } = state;
-
-    if (loading) return console.log('loading...'); if (error) return console.log('Error');
-
-    return(
-        <div>Home</div>
-    )
+    function handleLogout() {
+        signOut(auth).then(response => {
+            navigate('/login')
+            console.log(auth.currentUser)
+        })
     }
+
+  useMemo(() => {
+    const firebaseFunction = async () => {
+
+    const collectionReference = collection(db, "products")
+    const q = query(collectionReference, where('price', '<', 500), limit(2))
+    getDocs(q).then(response => {
+        response.forEach(price => {
+            console.log(price.data().price)
+        })
+    })
+    }
+
+    // firebaseFunction()
+  })
+
+  return(
+    <Button type="primary" onClick={handleLogout}>Logout</Button>
+  )
+}
 
 export default Home;
